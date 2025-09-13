90e35e2b8a670116d303f103eb95aa20ed9662a4
Azamat <azamat.rakhim@outlook.com>
2025-09-13 23:58:23 +0500

    h3_analyze.py // needs serious changes

--- src/data/h3_analyze.py
+++ src/data/h3_analyze.py
@@ -1,6 +1,29 @@
 import h3
 import pandas as pd
+import seaborn as sb
+import matplotlib as plt
 
 def analyze_geodata(df):
     df['h3_index'] = df.apply(lambda row: h3.geo_to_h3(row['lat'], row['lng'], 9), axis=1)
     return df
+
+def identify_popular_routes(df):
+    # Группировка по маршрутам и подсчет количества поездок
+    popular_routes = df.groupby(['start_h3_index', 'end_h3_index']).size().reset_index(name='count')
+    return popular_routes.sort_values(by='count', ascending=False)
+
+def plot_heatmap(df):
+    heatmap_data = df.pivot_table(index='lat', columns='lng', values='count', fill_value=0)
+    sns.heatmap(heatmap_data, cmap='YlGnBu')
+    plt.title('Тепловая карта спроса')
+    plt.show()
+
+def optimize_driver_distribution(df):
+    # Оптимизация распределения водителей на основе спроса
+    # Здесь можно использовать алгоритмы оптимизации
+    pass
+
+def safety_scenarios(df):
+    # Выявление необычных маршрутов и резких отклонений
+    unusual_routes = df[(df['spd'] > threshold_speed) | (df['alt'] > threshold_alt)]
+    return unusual_routes
